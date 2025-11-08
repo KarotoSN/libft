ce7c2e5a3355bc64560385c6e47ab409ad3f1e01
Abdel-Karim Arab <aarab@f2r8s7.clusters.42paris.fr>
2025-11-08 17:47:12 +0100

    bonus

---
+++ .history/bonus/ft_lstnew_20251108174139.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aarab <aarab@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/08 17:30:03 by aarab             #+#    #+#             */
+/*   Updated: 2025/11/08 17:41:40 by aarab            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list *New;
+
+	New = malloc(sizeof(t_list));
+	if (!New)
+		return (NULL);
+
+	New.content = content;
+	New.next = NULL;
+	return (New);
+}
